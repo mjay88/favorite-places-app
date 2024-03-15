b1e981b5fbabaa607a3f167bf79b43ed9b9b2803
Michael Jarrett <mwilliamkjarrett@gmail.com>
2024-03-15 15:58:40 -0500

    set up navigation and global styles

--- components/Places/PlacesList.jsx
+++ components/Places/PlacesList.jsx
@@ -1,7 +1,38 @@
-import { View, Text, FlatList } from "react-native";
+import { View, Text, FlatList, StyleSheet } from "react-native";
+import PlaceItem from "./PlaceItem";
+import { Colors } from "../../constants/colors";
 
 const PlacesList = ({ places }) => {
-	return <FlatList data={places} keyExtractor={(item) => item.id} />;
+	if (!places || places.length === 0) {
+		return (
+			<View style={styles.fallbackContainer}>
+				<Text style={styles.fallbackText}>
+					No places added yet - start adding some!
+				</Text>
+			</View>
+		);
+	}
+
+	return (
+		<FlatList
+			data={places}
+			keyExtractor={(item) => item.id}
+			renderItem={({ item }) => <PlaceItem place={item} />}
+		/>
+	);
 };
+
+const styles = StyleSheet.create({
+	fallbackContainer: {
+		justifyContent: "center",
+		alignItems: "center",
+		flex: 1,
+	},
+
+	fallbackText: {
+		fontSize: 16,
+		color: Colors.primary200,
+	},
+});
 
 export default PlacesList;
